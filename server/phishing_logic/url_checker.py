a3a81b40b79b4091b852c6cde4b22307067a04c9
Necrozzyz <necrozzycontact@proton.me>
2025-10-13 19:47:55 -0300

    Adicionando o time.sleep para requisições no whois, além da atualização na parte das importações para concordar com as mudanças no utils.py

--- server/phishing_logic/url_checker.py
+++ server/phishing_logic/url_checker.py
@@ -1,20 +1,12 @@
 import whois
 from urllib.parse import urlparse
 from datetime import datetime
-
-from .utils import carregar_lista, expandir_url
-
-def verificar_palavras_chave(url: str, palavras_suspeitas: list[str]) -> str | None:
-    if any(p in url.lower() for p in palavras_suspeitas):
-        return "Contém palavras suspeitas" 
-    return None
-
-def verificar_tld_suspeito(dominio: str, tlds_suspeitos: list[str]) -> str | None:
-    if any(dominio.endswith(tld) for tld in tlds_suspeitos):
-        return "Domínio de nível superio (TLD) suspeito ou incomum"
-    return None
+import time
+import utils
 
 def verificar_dominio_recente(dominio: str) -> str | None:
+    time.sleep(1.0)
+    
     try:
         info = whois.whois(dominio)
         if info and info.creation_date:
@@ -43,7 +35,7 @@ def verificar_encurtadores(url: str, dominio: str, encurtadores: list[str]) -> s
     if dominio in encurtadores:
         motivo = ["URL encurtada"]
         # Tenta expandir para verificar destino final
-        expandida = expandir_url(url)
+        expandida = utils.expandir_url(url)
         if expandida and expandida != url:
             motivo.append(f"URL expandida (destino final): {expandida}")
         return "; ".join(motivo)
@@ -54,9 +46,9 @@ def verificar_parametros_longos(url: str) -> str | None:
     return None
 
 def analisar_url(url: str) -> dict:
-    palavras_suspeitas = carregar_lista("palavras_suspeitas.txt")
-    tlds_suspeitos = carregar_lista("tlds_suspeitos.txt")
-    encurtadores = carregar_lista("encurtadores.txt")
+    palavras_suspeitas = utils.carregar_lista("palavras_suspeitas.txt")
+    tlds_suspeitos = utils.carregar_lista("tlds_suspeitos.txt")
+    encurtadores = utils.carregar_lista("encurtadores.txt")
 
     heuristicas = []
 
@@ -65,8 +57,8 @@ def analisar_url(url: str) -> dict:
         dominio = parsed.netloc.lower()
 
         verficacoes = [
-            verificar_palavras_chave(url, palavras_suspeitas),
-            verificar_tld_suspeito(url, tlds_suspeitos),
+            utils.verificar_palavras_chave(url, palavras_suspeitas),
+            utils.verificar_tld_suspeito(url, tlds_suspeitos),
             verificar_dominio_recente(dominio),
             verificar_encurtadores(url, dominio, encurtadores),
             verificar_parametros_longos(url),
