51a3a2c2183f50f4917de01eecdd09507e4773bb
Necrozzyz <necrozzycontact@proton.me>
2025-11-10 20:38:17 -0300

    Corrigindo erro de digitação

--- server/phishing_logic/__init__.py
+++ server/phishing_logic/__init__.py
@@ -4,14 +4,15 @@ from .email_checker import analisar_email
 __all__ = ["analisar_texto"]
 
 def analisar_texto(texto: str, tipo: str) -> dict:
+    """Função central que delega a análise conforme o tipo (URL ou e-mail)."""
     texto = texto.strip()
 
     if not texto:
-        return {"suspicius": False, "reason": "Entrada vazia"}
+        return {"suspicious": False, "reason": "Entrada vazia"}  
     
     if tipo == "url":
         return analisar_url(texto)
     elif tipo == "email":
         return analisar_email(texto)
     else:
-        return  {"suspicious": True, "reason": f"Tipo de entrada não reconhecido: {tipo}"}
+        return {"suspicious": True, "reason": f"Tipo de entrada não reconhecido: {tipo}"}
