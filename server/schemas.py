f5f9994615750a57373f7ee248dc384521e17e0f
Necrozzyz <necrozzycontact@proton.me>
2025-12-02 22:23:50 -0300

    ?

--- server/schemas.py
+++ server/schemas.py
@@ -1,8 +1,6 @@
 from marshmallow import Schema, fields, validate
 
-# Schema de resposta: O formato que o endpoint /analisar/ retorna para CADA ITEM
 class PhishingResponseSchema(Schema):
-    # NOVO CAMPO: O item analisado (URL ou e-mail) que o usuário solicitou
     analysed_item = fields.Str(
         required=True,
         metadata={"description": "O item (URL ou e-mail) que foi submetido à análise."}
@@ -16,7 +14,7 @@ class PhishingResponseSchema(Schema):
         metadata={"description": "Detalhes dos indicadores encontrados."}
     )
 
-# Schema de entrada: O formato esperado no body da requisição POST
+
 class SimpleBatchCheckerSchema(Schema):
     tipo_geral = fields.Str(
         required=True, 
